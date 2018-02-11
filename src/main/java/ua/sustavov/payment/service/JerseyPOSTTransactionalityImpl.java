250a79b461159b2b2355ad5cd77309f19249ac36
Sustavov Anton <sustav86@gmail.com>
2018-02-11 20:14:41 +0200

    Modify sale method, post impl

--- src/main/java/ua/sustavov/payment/service/JerseyPOSTTransactionalityImpl.java
+++ src/main/java/ua/sustavov/payment/service/JerseyPOSTTransactionalityImpl.java
@@ -1,5 +1,7 @@
 package ua.sustavov.payment.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Joiner;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.springframework.stereotype.Service;
@@ -7,8 +9,10 @@ import org.zkoss.zk.ui.Executions;
 import ua.sustavov.payment.model.Card;
 import ua.sustavov.payment.model.Transaction;
 import com.sun.jersey.api.client.Client;
+import ua.sustavov.payment.model.User;
 
 import javax.ws.rs.core.MediaType;
+import java.util.Map;
 
 /**
  * Created by SUSTAVOV on 08.02.2018.
@@ -19,14 +23,29 @@ public class JerseyPOSTTransactionalityImpl implements Transactionality {
 
     private static String BASE_URL = "https://sandbox-secure.unitedthinkers.com/gates/xurl";
 
+    private AuthenticationService authenticationService = new AuthenticationServiceImpl();
+
     @Override
     public String sale(Card card, Transaction transaction) {
 
         Client client = Client.create();
         WebResource webResource = client.resource(BASE_URL);
+        User currentUser = authenticationService.getCurrentUser();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, String> map = objectMapper.convertValue(transaction, Map.class);
+        map.put("userName", currentUser.getUserName());
+        map.put("password", currentUser.getPassword());
+        map.put("requestType", map.getOrDefault("requestType", "sale").toLowerCase());
+
+        StringBuilder mapJoined = Joiner.on("&").useForNull("").withKeyValueSeparator("=")
+                .appendTo(new StringBuilder(), map);
 
         ClientResponse response = webResource.type(MediaType.APPLICATION_FORM_URLENCODED)
-                .post(ClientResponse.class, transaction.toPOSTString());
+                .post(ClientResponse.class, mapJoined.toString());
+
+        System.out.println(response);
 
         if (response.getStatus() == 200) {
             Executions.sendRedirect("/pages/success.zul");
